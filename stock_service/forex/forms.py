1fd78a2bb12a3b46ed202f375749366aa0c524ab
Andres Plazas <acpr87@gmail.com>
2021-12-18 11:59:12 +0100

    Add a CustomCreationOrderForm to allow insert orders manually using the admin.

--- stock_service/forex/forms.py
+++ stock_service/forex/forms.py
@@ -3,6 +3,13 @@ from django.forms import ModelForm
 from stock_service.forex.models import Order
 
 
+class CustomOrderCreateForm(ModelForm):
+    class Meta:
+        """Meta class"""  # noqa: D401
+        model = Order
+        fields = ["id", "type", "px", "quantity"]
+
+
 class CustomOrderChangeForm(ModelForm):
     class Meta:
         """Meta class"""  # noqa: D401
