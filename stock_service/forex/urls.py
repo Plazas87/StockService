94b19975732ff5eb09fa04c89661976d29718218
Andres Plazas <acpr87@gmail.com>
2021-12-21 21:29:34 +0100

    Improve the URLs

--- stock_service/forex/urls.py
+++ stock_service/forex/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from stock_service.forex.views import BidsView, AsksView, AskBidTotalView
 
 urlpatterns = [
-    path("general/<str:ticker>/bids", BidsView.as_view()),
-    path("general/<str:ticker>/asks", AsksView.as_view()),
-    path("general/<str:ticker>/totals", AskBidTotalView.as_view()),
+    path("<str:ticker>/bids", BidsView.as_view()),
+    path("<str:ticker>/asks", AsksView.as_view()),
+    path("<str:ticker>/totals", AskBidTotalView.as_view()),
 ]
